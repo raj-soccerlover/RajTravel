3d929df517dc584899ceeedc81074d4ff277b6bf
Abhinav Raj <raj.soccerlover@gmail.com>
2020-04-06 17:33:08 +0530

    second commit

--- js/main.js
+++ js/main.js
@@ -26,6 +26,21 @@ e.preventDefault();
 
 });
 
+$("#cover .col-sm-3 a[href^='#']").on('click', function (e) {
+
+    e.preventDefault();
+
+    var hash = this.hash;
+
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top
+    }, 300, function () {
+
+        window.location.hash = hash;
+    });
+
+});
+
 $(".footer .scroll-to-top-button a[href^='#']").on('click', function (e) {
 
     e.preventDefault();
